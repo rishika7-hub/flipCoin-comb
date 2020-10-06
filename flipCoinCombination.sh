7cc13b266e1b0529e38d4f1ae9a8451ea0b0540d
rishika7-hub <srishika970@gmail.com>
2020-10-06 23:35:25 +0530

    Added triplet and max percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -133,3 +133,168 @@ doublet_program ()
 
 	echo "Maximum Percentage of $maxStr is $maxPercent "
 }
+
+triplet_program ()
+{
+        declare -A triplet
+        HHHCount=0;
+        TTTCount=0;
+        HHTCount=0;
+        HTHCount=0;
+	HTTCount=0;
+	TTHCount=0;
+	THTCount=0;
+	THHCount=0;
+
+        read -p "enter number of times to flip coin = " num
+        echo -e
+        for i in `seq $num`
+        do
+                flip1=$((1+RANDOM%2))
+                flip2=$((1+RANDOM%2))
+		flip3=$((1+RANDOM%2))
+                coin=$flip1$flip2$flip3
+                case $coin in
+                        111)     ((HHHCount++))
+                                triplet[HHH]=$HHHCount ;;
+                        222)     ((TTTCount++))
+                                triplet[TTT]=$TTTCount ;;
+                        112)     ((HHTCount++))
+                                triplet[HHT]=$HHTCount ;;
+                        121)     ((HTHCount++))
+                                triplet[HTH]=$HTHCount ;;
+			122)	 ((HTTCount++))
+				triplet[HTT]=$HTTCount ;;
+			221)	((TTHCount++))
+				triplet[TTH]=$TTHCount ;;
+			212)	((THTCount++))
+				triplet[THT]=$THTCount ;;
+			211)	((THHCount++))
+				triplet[THH]=$THHCount ;;
+                esac
+        done
+
+        echo "print triplet dictionary"
+
+        HHHPercent=0;
+        TTTPercent=0;
+
+        HHTPercent=0;
+        HTHPercent=0;
+	HTTPercent=0;
+
+	TTHPercent=0;
+	THTPercent=0;
+	THHPercent=0;
+
+        maxPercent=0;
+        maxStr=w
+
+        for key in "${!triplet[@]}"
+        do
+                echo "$key : ${triplet[$key]}"
+                if [ "$key" = "HHH" ]
+                then
+                        a=$(awk 'BEGIN {print '${triplet[$key]}' / '$num' }')
+                        HHHPercent=$(awk 'BEGIN {print '$a' * '100' }')
+                        if (( ${HHHPercent%%.*} > ${maxPercent%%.*} ))
+                        then
+                                maxPercent=$HHHPercent
+                                maxStr=HHH
+                        fi
+                elif [ "$key" = "TTT" ]
+                then
+                        b=$(awk 'BEGIN {print '${triplet[$key]}' / '$num' }')
+                        TTTPercent=$(awk 'BEGIN {print '$b' * '100' }')
+                        if [ ${TTTPercent%%.*} -gt ${maxPercent%%.*} ]
+                        then
+                                maxPercent=$TTTPercent
+                                maxStr=TTT
+                        fi
+                elif [ "$key" = "HHT" ]
+                then
+                        c=$(awk 'BEGIN {print '${triplet[$key]}' / '$num' }')
+                        HHTPercent=$(awk 'BEGIN {print '$c' * '100' }')
+                        if [ ${HHTPercent%%.*} -gt ${maxPercent%%.*} ]
+                        then
+                                maxPercent=$HHTPercent
+                                maxStr=HHT
+                        fi
+                elif [ "$key" = "HTH" ]
+                then
+                        b=$(awk 'BEGIN {print '${triplet[$key]}' / '$num' }')
+                        HTHPercent=$(awk 'BEGIN {print '$b' * '100' }')
+                        if [ ${HTHPercent%%.*} -gt ${maxPercent%%.*} ]
+                        then
+                                maxPercent=$HTHPercent
+                                maxStr=HTH
+                        fi
+                elif [ "$key" = "HTT" ]
+                then
+                        c=$(awk 'BEGIN {print '${triplet[$key]}' / '$num' }')
+                        HTTPercent=$(awk 'BEGIN {print '$c' * '100' }')
+                        if [ ${HTTPercent%%.*} -gt ${maxPercent%%.*} ]
+                        then
+                                maxPercent=$HTTPercent
+                                maxStr=HTT
+                        fi
+                elif [ "$key" = "TTH" ]
+                then
+                        b=$(awk 'BEGIN {print '${triplet[$key]}' / '$num' }')
+                        TTHPercent=$(awk 'BEGIN {print '$b' * '100' }')
+                        if [ ${TTHPercent%%.*} -gt ${maxPercent%%.*} ]
+                        then
+                                maxPercent=$TTHPercent
+                                maxStr=TTH
+                        fi
+                elif [ "$key" = "THT" ]
+                then
+                        c=$(awk 'BEGIN {print '${triplet[$key]}' / '$num' }')
+                        THTPercent=$(awk 'BEGIN {print '$c' * '100' }')
+                        if [ ${THTPercent%%.*} -gt ${maxPercent%%.*} ]
+                        then
+                                maxPercent=$THTPercent
+                                maxStr=THT
+                        fi
+
+                else
+                        d=$(awk 'BEGIN {print '${triplet[$key]}' / '$num' }')
+                        THHPercent=$(awk 'BEGIN {print '$d' * '100' }')
+                        if [ ${THHPercent%%.*} -gt ${maxPercent%%.*} ]
+                        then
+                                maxPercent=$THHPercent
+                                maxStr=THH
+                        fi
+                fi
+        done
+
+        echo "Percentage of HHH = $HHHPercent"
+	echo -e
+        echo "Percentage of TTT = $TTTPercent"
+        echo -e
+        echo "Percentage of HHT = $HHTPercent"
+	echo -e
+        echo "Percentage of HTH = $HTHPercent"
+        echo -e
+        echo "Percentage of HTT = $HTTPercent"
+        echo -e
+        echo "Percentage of TTH = $TTHPercent"
+        echo -e
+        echo "Percentage of THT = $THTPercent"
+        echo -e
+        echo "Percentage of THH = $THHPercent"
+
+
+        echo -e
+
+        echo "Maximum Percentage of $maxStr is $maxPercent "
+
+
+}
+
+read -p "choose 1) singlet 2) doublet 3) triplet ==>  " number
+case $number in
+        1) singlet_program ;;
+        2) doublet_program ;;
+	3) triplet_program ;;
+esac
